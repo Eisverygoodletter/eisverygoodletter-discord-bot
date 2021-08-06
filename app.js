06bc1ef34560aa4c82aec96b564b4ae0e8990b9a
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-08-06 20:06:57 +0800

    fix on 60 second fail

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const http = require("http");
 client.on('ready', () => {
   console.log(`Logged in...`);
 });
@@ -12,3 +12,8 @@ client.on('message', msg => {
 });
 
 client.login('ODczMTU2OTE3NDA3NjY2MTg2.YQ0Uvw.d4FTiklG5A3J1XN0HtY7uN-v0GA');
+
+http.createServer(function(request, response){
+    console.log(request,response);
+
+}).listen(process.env.PORT || 5000);
