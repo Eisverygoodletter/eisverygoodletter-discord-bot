a31b53aa18092c3719368a6629904fb50b477133
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-08-06 19:41:56 +0800

    initial setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in...`);
+});
+
+client.on('message', msg => {
+  
+});
+
+client.login('ODczMTU2OTE3NDA3NjY2MTg2.YQ0Uvw.d4FTiklG5A3J1XN0HtY7uN-v0GA');
