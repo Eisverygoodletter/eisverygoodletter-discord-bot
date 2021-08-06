4d56a490f38b104f7461d2ea180c799e4997b83a
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-08-06 19:56:35 +0800

    first command

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  
+  if(msg.content.startsWith("!")){
+      msg.reply("why tf you wake me up");
+  }
 });
 
 client.login('ODczMTU2OTE3NDA3NjY2MTg2.YQ0Uvw.d4FTiklG5A3J1XN0HtY7uN-v0GA');
