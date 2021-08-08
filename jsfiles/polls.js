87d26055e358697ff707b12c103f7dddc2b5fe7a
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-08-08 09:13:14 +0800

    poll update

--- jsfiles/polls.js
+++ jsfiles/polls.js
@@ -1,5 +1,21 @@
+const charList = "🇦🇧🇨🇩🇪";
+
 function poll(inputArray, msg){
     console.log("wow poll");
+    // index 0 in inputArray is just the command, actual stuff starts from 1
+    var channel = msg.channel;
+    if(inputArray.length >= 5){
+        channel.send("ERROR: Too many parameters");
+    }
+    var messageToSend = ""
+    messageToSend += "vote!\n";
+    for(let i = 1; i < inputArray.length; i++){
+        // start from 1 bc 0 is the command
+        var indic = charList[i - 1];
+        var addIn = indic + ": " + inputArray[i] + "\n";
+        messageToSend += addIn;
+    }
+    channel.send(messageToSend);
 }
 
 
