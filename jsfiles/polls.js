3f51d9636a96fa8c9b6d384f1e4112d04537d458
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-08-08 10:15:10 +0800

    f

--- jsfiles/polls.js
+++ jsfiles/polls.js
@@ -4,16 +4,15 @@ function poll(inputArray, msg){
     console.log("wow poll");
     // index 0 in inputArray is just the command, actual stuff starts from 1
     var channel = msg.channel;
-    if(inputArray.length >= 5){
-        channel.send("ERROR: Too many parameters");
-    }
     var messageToSend = ""
     messageToSend += inputArray[1] + "\n";
     messageToSend += "vote!\n";
-    for(let i = 2; i < inputArray.length; i++){
+    var ansText = inputArray.slice(2).join();
+    var ansArray = ansText.split(",");
+    for(let i = 0; i < ansArray.length; i++){
         // start from 1 bc 0 is the command
-        var indic = ":regional_indicator_" + String.fromCharCode(i + 95); // 97 starts with a
-        var addIn = indic + ": " + inputArray[i] + "\n";
+        var indic = ":regional_indicator_" + String.fromCharCode(i + 97); // 97 starts with a
+        var addIn = indic + ": " + ansArray[i] + "\n";
         messageToSend += addIn;
     }
     channel.send(messageToSend);
