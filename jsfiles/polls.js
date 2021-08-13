d11cb8acac386389e48344ee699fc9166c9b1cf8
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-08-13 20:26:13 +0800

    final fix

--- jsfiles/polls.js
+++ jsfiles/polls.js
@@ -19,10 +19,8 @@ function poll(params, msg, client){
     if(weSentSomething){
         msg.channel.send(messageText).then((newMsg)=>{
             // add reactions
-            console.log(client.emojis.cache);
             for(let i = 1; i < params.length; i++){
-                console.log(msg.guild.emojis.cache.find(emoji => emoji.name === ":regional_indicator_"+String.fromCharCode(i + 96)))
-                newMsg.react('🇦');
+                newMsg.react(charList[i - 1]);
             }
         })
     }
