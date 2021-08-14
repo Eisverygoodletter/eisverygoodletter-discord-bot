9e5cb1db05bdfeb61dd78c4526cd21c784295a40
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-08-14 15:38:17 +0800

    firestore initialize, modal added

--- intercon/intercon.js
+++ intercon/intercon.js
@@ -1,11 +1,24 @@
 
 
 module.exports = function(app, client){
+    var admin = require("firebase-admin");
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.FIRESTORE_PROJECT_ID,
+            clientEmail: process.env.FIRESTORE_CLIENT_EMAIL,
+            privateKey: process.env.FIRESTORE_PRIVATE_KEY,
+        }),
+        databaseURL: process.env.FIRESTORE_DATABASE_URL,
+        projectId: process.env.FIRESTORE_PROJECT_ID
+    });
     // first, define the webpage send
     app.get("/intercon", function(req,res){
         res.sendFile("./intercon.html", {root: __dirname});
     });
     app.get("/INTERCON/LOGIN", function(req, res){
         
-    })
+    });
+    app.get("/INTERCON/CREATE_ACC", function(req, res){
+
+    });
 }
---
+++ intercon/interconclient.js
@@ -0,0 +1,6 @@
+// jquery allowed in here
+
+$(document).ready(function(){
+    // put all code in here
+    $("#loginModal").modal("show");
+})
