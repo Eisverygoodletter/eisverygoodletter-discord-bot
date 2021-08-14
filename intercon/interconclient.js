cd010bc6721ecf1906addd29955a47aec4a447af
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-08-14 17:20:54 +0800

    client side updates

--- intercon/interconclient.js
+++ intercon/interconclient.js
@@ -3,4 +3,24 @@
 $(document).ready(function(){
     // put all code in here
     $("#loginModal").modal("show");
+    $("#login").onclick = function(){
+        if($("#modalUsernameInput").val() == ""){
+            alert("error: you need to put in a username");
+        }
+        else if($("#modalPasswordInput").val() == ""){
+            alert("error: you need to put in a password");
+        }
+        else{
+            if($("#modalNewInput").val() == true){
+                // create a new account
+                console.log("requesting for creating a new account");
+            }
+            else{
+                // login to the old account
+                console.log("requesting for log in...");
+            }
+            // clean this function off
+            $("#login").onclick = null;
+        }
+    }
 })
