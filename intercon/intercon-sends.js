bd98bc96ed9f346c13226e5d1ea75ccad9158988
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-08-25 14:01:57 +0800

    added ping token check

--- intercon/intercon-sends.js
+++ intercon/intercon-sends.js
@@ -1,13 +1,27 @@
 
 module.exports = function (app, client){
-    app.post("/INTERCON/PING-TOKEN", (req, res)=>{
-        console.log(req.cookies);
+    app.post("/INTERCON/PING-TOKEN", async (req, res)=>{
+        //console.log(req.cookies);
+        const clientToken = req.cookies[process.env.tokenCookie];
+        // check if such a token exists in the global
+        const tokenExist = global.tokenList.some((element) => element.token == clientToken);
         var returnInfo;
-        returnInfo = {
-            succeeded: true,
-            returnCode: 200,
-            returnText: "success"
-        };
+        if(tokenExist){
+            var tokenObj = global.tokenList.find((element) => element.token == clientToken);
+            tokenObj.pinged = true;
+            returnInfo = {
+                succeeded: true,
+                returnCode: 200,
+                returnText: "success"
+            };
+        }
+        else{
+            returnInfo = {
+                succeeded: false,
+                returncode: 418,
+                returnText: "token doesnt exist. Session timeout",
+            };
+        }
         res.send(returnInfo);
     });
 }
