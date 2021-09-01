53864ae1ab3789b7ea24894bc6deb2e659702081
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-09-01 08:30:50 +0800

    remove useless include

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const http = require("http");
 global.http = http;
 const fs = require("fs");
 global.fs = fs;
-const request = require("request");
 const verifyJS = require("./jsfiles/verify.js");
 global.verifyJS = verifyJS;
 const express = require("express");
