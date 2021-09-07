4beaebcb6d4fded3c0dc0190b3d4eb6cb61a2793
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-09-07 17:27:28 +0800

    yay

--- intercon/clientSide/interconclientBuildChannelUI.js
+++ intercon/clientSide/interconclientBuildChannelUI.js
@@ -1,3 +1,38 @@
-function generateChannelUI(info){
+async function generateChannelUI(info){
     console.log(info);
+    // first, get a new channel/server list from INTERCON/GET/SERVERLIST
+    const result = await getPost("INTERCON/GET/SERVERLIST", {});
+    // check if result succeeds
+    if(result.succeeded == false){
+        alert("error " + result.returnCode + ", reason: " + result.returnText);
+        location.reload();
+        return;
+    }
+    // clear entire current channel list
+    $("#channebarGroup").empty();
+    // get a list of the required channelIds
+    var channelIdList = [];
+    for(let i = 0; i < result.returnData.length; i++){
+        if(result.returnData[i].serverId == info){
+            channelIdList.push(result.returnData[i].channelId);
+        }
+    }
+    console.log(channelIdList);
+    // get list of channel names
+    var channelNames = [];
+    for(let i = 0; i < channelIdList.length; i++){
+        const res = await getPost("INTERCON/GET/CHANNELNAME", {serverId: info, channelId: channelIdList[i]});
+        if(res.succeeded == false){
+            alert("oof");
+            break;
+        }
+        channelNames.push(res.returnData);
+    }
+    // generate elements for each channel name
+    console.log(channelNames);
+    for(let i = 0; i < channelNames.length; i++){
+        var element = document.createElement("button");
+        element.textContent = channelNames[i];
+        $("#channelbarGroup").append(element);
+    }
 }
