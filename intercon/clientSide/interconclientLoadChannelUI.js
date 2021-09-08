23c3abd9f2cdce802f65df23dc7fabf8649bbd15
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-09-08 08:49:28 +0800

    fricc

--- intercon/clientSide/interconclientLoadChannelUI.js
+++ intercon/clientSide/interconclientLoadChannelUI.js
@@ -1,3 +1,16 @@
 async function loadChannelUI(info){
-    console.log(info);
+    
 }
+
+async function sendMsg(event){
+    console.log($("#sendText").val());
+}
+
+$(document).ready(()=>{
+    $("#sendText").on("keyup", (e)=>{
+        if(e.key === "Enter" || e.keyCode === 13){
+            const sendText = $("#sendText").val();
+            $("#sendText").val(" ");
+        }
+    });
+});
