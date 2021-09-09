db9baaeb29551017f242cc55cc8015840718bf49
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-09-09 11:22:17 +0800

    frick 2 rebooted

--- intercon/clientSide/interconclientLoadChannelUI.js
+++ intercon/clientSide/interconclientLoadChannelUI.js
@@ -11,13 +11,14 @@ async function loadChannelUI(info){
     $("#textList").html("");
     console.log(messages);
     // turn them into understandable html elements
-    for(let i = 0; i < messages.length; i++){
+    for(let i = messages.length - 1; i >= 0; i--){
         var element = document.createElement("a");
         element.href = "#";
         element.classList.add("border", "list-group-item", "list-group-action", "disabled", "bg-dark");
         element.textContent = messages[i].author + ": " + messages[i].content;
         $("#textList").append(element);
     }
+    $("#textList").scrollTop($("#textList")[0].scrollHeight);
 }
 
 async function sendMsg(event){
