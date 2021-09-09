d21df28fd5d11c708bc03df9a77a0eec168ca206
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-09-09 08:34:32 +0800

    frick 2

--- intercon/clientSide/interconclientLoadChannelUI.js
+++ intercon/clientSide/interconclientLoadChannelUI.js
@@ -15,7 +15,7 @@ async function loadChannelUI(info){
         var element = document.createElement("a");
         element.href = "#";
         element.classList.add("border", "list-group-item", "list-group-action", "disabled", "bg-dark");
-        element.textContent = messages[i].author + ": " + element[i].content;
+        element.textContent = messages[i].author + ": " + messages[i].content;
         $("#textList").append(element);
     }
 }
