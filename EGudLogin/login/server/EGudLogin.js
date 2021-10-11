dbc9d6c097cb38ae133eedb72dc1567b0a7adf74
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-10-11 20:41:21 +0800

    bruh

--- EGudLogin/login/server/EGudLogin.js
+++ EGudLogin/login/server/EGudLogin.js
@@ -2,7 +2,7 @@ var router = global.express.Router();
 
 
 async function sendLogin(req, res){
-    res.sendFile("../client/login.html");
+    res.sendFile("../client/login.html", {root: __dirname});
 }
 
 router.get("/", sendLogin);
