d4431fa8adae4dd3f8168948c8b3a51bd6aad8b6
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-10-14 08:30:19 +0800

    some code

--- EGudLogin/login/client/loginClient.js
+++ EGudLogin/login/client/loginClient.js
@@ -1,5 +1,17 @@
 var accountCreationButton = document.getElementById("accountCreate");
-accountCreationButton.onsubmit = function(){
+accountCreationButton.onclick = function(){
     console.log("going to account creation page");
-    window.location.href = location.href + "/createAccount";
+    location.href = location.href + "/createAccount";
+}
+
+var accountLoginButton = document.getElementById("accountLogin");
+var usernameInput = document.getElementById("username");
+var passwordInput = document.getElementById("password");
+accountLoginButton.onclick = function(){
+    console.log("Logging in...")
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    const hashingSalt = bcrypt.genSaltSync(5);
+    const hashedPassword = bcrypt.hashSync(password, hashingSalt);
+    console.log(username, hashedPassword);
 }
