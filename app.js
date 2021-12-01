a786ac5c436e7b07eb79e0d6b30bf630b922733f
Eisverygoodletter <yuetlongfeng@gmail.com>
2021-12-01 08:45:24 +0800

    middleware support ????

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const corsOptions = {
-  origin: JSON.parse(process.env.JSON_ORIGIN),
-  optionSuccessStatus: 200,
+    origin: JSON.parse(process.env.JSON_ORIGIN),
+    optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -35,68 +35,67 @@ var server = app.listen(process.env.PORT || 5000);
 
 var currentVerifying = []
 var commandList = []; // in the format of {commandName:___, details:{}}
-
+global.middleware = [];
 client.on('ready', () => {
-  console.log(`Logged in...`);
+    console.log(`Logged in...`);
 });
 
 client.on('message', msg => {
-  if(msg.channel.name == process.env.VERIFY_CHANNEL){
-    if(msg.content = "!verify"){
-      
-      var linkId = verifyJS.verify(msg, client);
-      app.get(linkId.link, function(req,res){
-        res.sendFile("./jsfiles/verify.html", {root: __dirname});
-      });
-      app.post(linkId.link, function(req, res){
-        console.log("we dun");
-        var verifiedRole = linkId.msg.member.guild.roles.cache.find(role => role.name === "verified");
-        linkId.msg.member.roles.add(verifiedRole);
-        client.channels.cache.get(process.env.GENERAL_CHANNEL_KEY).send(linkId.msg.author.username + " just got verified! welcome!");
-      });
-      currentVerifying.push(linkId);
+    for (let middleware of global.middleware) {
+        msg = middleware(msg, client);
     }
-  }
-  else{
-    var command = msg.content.split(" ")[0]; //first word
-    for(var i = 0; i < commandList.length; i++){
-      if(command == commandList[i].commandName){
-        // check if person has role
-        var hasPerms = [];
-        for(var j = 0; j < commandList[i].details.requiredPerms.length; j++){
-          if(msg.member.roles.cache.some(role => role.name === commandList[i].details.requiredPerms[j])){
-            hasPerms.push(true);
-          }
-          else{
-            hasPerms.push(false);
-          }
+    if (msg.channel.name == process.env.VERIFY_CHANNEL) {
+        if (msg.content = "!verify") {
+            var linkId = verifyJS.verify(msg, client);
+            app.get(linkId.link, function(req, res) {
+                res.sendFile("./jsfiles/verify.html", { root: __dirname });
+            });
+            app.post(linkId.link, function(req, res) {
+                console.log("we dun");
+                var verifiedRole = linkId.msg.member.guild.roles.cache.find(role => role.name === "verified");
+                linkId.msg.member.roles.add(verifiedRole);
+                client.channels.cache.get(process.env.GENERAL_CHANNEL_KEY).send(linkId.msg.author.username + " just got verified! welcome!");
+            });
+            currentVerifying.push(linkId);
         }
-        var canCommand = false;
-        if(commandList[i].details.requireAllPermsListed == true){
-          // all bools in hasPerms must be true
-          canCommand = hasPerms.every((e)=>{
-            return e == true;
-          });
+    } else {
+        var command = msg.content.split(" ")[0]; //first word
+        for (var i = 0; i < commandList.length; i++) {
+            if (command == commandList[i].commandName) {
+                // check if person has role
+                var hasPerms = [];
+                for (var j = 0; j < commandList[i].details.requiredPerms.length; j++) {
+                    if (msg.member.roles.cache.some(role => role.name === commandList[i].details.requiredPerms[j])) {
+                        hasPerms.push(true);
+                    } else {
+                        hasPerms.push(false);
+                    }
+                }
+                var canCommand = false;
+                if (commandList[i].details.requireAllPermsListed == true) {
+                    // all bools in hasPerms must be true
+                    canCommand = hasPerms.every((e) => {
+                        return e == true;
+                    });
+                } else {
+                    // just need 1 perm
+                    canCommand = hasPerms.some((e) => {
+                        return e == true;
+                    });
+                }
+                if (canCommand) {
+                    var params = null;
+                    if (commandList[i].details.paramType === "infi-size") {
+                        params = msg.content.substr(msg.content.indexOf(" ") + 1);
+                        // params now does not have the poll
+                        params = params.split(commandList[i].details.splittingUnit);
+                    }
+                    commandList[i].details.callFunc(params, msg, client);
+                }
+                break; // no other commands will match
+            }
         }
-        else{
-          // just need 1 perm
-          canCommand = hasPerms.some((e)=>{
-            return e == true;
-          });
-        }
-        if(canCommand){
-          var params = null;
-          if(commandList[i].details.paramType === "infi-size"){
-            params = msg.content.substr(msg.content.indexOf(" ") + 1);
-            // params now does not have the poll
-            params = params.split(commandList[i].details.splittingUnit);
-          }
-          commandList[i].details.callFunc(params, msg, client);
-        }
-        break; // no other commands will match
-      }
     }
-  }
 });
 
 client.login(process.env.BOT_TOKEN);
@@ -111,16 +110,16 @@ client.login(process.env.BOT_TOKEN);
 
 */
 
-function loadCommands(){
-  commandList = commandList.concat(require("./jsfiles/polls.js")());
-  require("./intercon/intercon.js")(app, client);
-  commandList = commandList.concat(require("./intercon/intercon-allows.js")());
-  app.use("/chess", require("./chess/serverSide/chess.js"));
-  app.use("/game", require("./EGudLogin/EGudMain.js"));
-  commandList = commandList.concat(require("./dumbMark/dumbMark.js")());
+function loadCommands() {
+    commandList = commandList.concat(require("./jsfiles/polls.js")());
+    require("./intercon/intercon.js")(app, client);
+    commandList = commandList.concat(require("./intercon/intercon-allows.js")());
+    app.use("/chess", require("./chess/serverSide/chess.js"));
+    app.use("/game", require("./EGudLogin/EGudMain.js"));
+    commandList = commandList.concat(require("./dumbMark/dumbMark.js")());
 }
 loadCommands();
 
-app.get("/", async (req, res)=>{
-  res.send("Welcome to the root directory! there's nothing here :)")
+app.get("/", async(req, res) => {
+    res.send("Welcome to the root directory! there's nothing here :)")
 });
--- dumbMark/dumbMark.js
+++ dumbMark/dumbMark.js
@@ -1,42 +1,41 @@
 global.dumbMarkJS = {}
 global.dumbMarkJS.markedIds = [];
-function markUser(params, msg, client){
+
+function markUser(params, msg, client) {
     let mentionedUsers = msg.mentions;
     let members = msg.members;
     let channel = msg.channel;
     channel.send("marking");
-    for(let user in members){
+    for (let user in members) {
         console.log(user.name);
-        if(!(user.id in global.dumbMarkJS.markedIds)){
+        if (!(user.id in global.dumbMarkJS.markedIds)) {
             global.dumbMarkJS.markedIds.push(user.id);
-        }
-        else
+        } else
             channel.send("one of these users are already on the dum list");
     }
 }
 
-function unMarkUser(params, msg, client){
+function unMarkUser(params, msg, client) {
     let mentionedUser = msg.mentions;
     let members = msg.members;
     let channel = msg.channel;
     let removalList = [];
     channel.send("unmarking");
-    for(let user of members){
+    for (let user of members) {
         console.log("removing");
         console.log(user.id);
-        if(user.id in global.dumbMarkJS.markedIds){
+        if (user.id in global.dumbMarkJS.markedIds) {
             removalList.push(user.id);
-        }
-        else
+        } else
             channel.send("this user isn't in the dum list ????");
     }
-    global.dumbMarkJS.markedIds = global.dumbMarkJS.markedIds.filter((a)=> !(a in removalList));
+    global.dumbMarkJS.markedIds = global.dumbMarkJS.markedIds.filter((a) => !(a in removalList));
 }
 
-module.exports = function(app, client){
+module.exports = function(app, client) {
     let mark = {
         commandName: "E_dumark",
-        details:{
+        details: {
             callFunc: markUser,
             requiredPerms: [],
             requireAllPermsListed: true,
@@ -46,7 +45,7 @@ module.exports = function(app, client){
     }
     let unmark = {
         commandName: "E_undumark",
-        details:{
+        details: {
             callFunc: unMarkUser,
             requiredPerms: [],
             requireAllPermsListed: true,
@@ -56,3 +55,10 @@ module.exports = function(app, client){
     }
     return [mark, unmark];
 }
+
+function dumbMarkMiddleware(msg, client) {
+    if (msg.author.id in global.dumbMarkJS.markedIds) {
+        msg.channel.send("REEEEEEEEEEEEE am broken bot pls fix pls pls pls pls");
+    }
+}
+global.middleware.push(dumbMarkMiddleware);
